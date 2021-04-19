a759d3687c5a646120dbecc1148ad7247eb339e7
Freddy T <fthomp2430@gmail.com>
2021-04-19 14:37:55 -0400

    frontend debug

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
 from . import views
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-)
+
 urlpatterns = [
     path('users/login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
 
