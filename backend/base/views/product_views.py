90f08e477ec00b4ae9afced4d1ff9f783b2f028c
fthomp8044 <fthomp2430@gmail.com>
2021-06-14 22:55:47 -0400

    created creeat and update product enpoints and created the admin create product

--- backend/base/views/product_views.py
+++ backend/base/views/product_views.py
@@ -34,3 +34,45 @@ def getProduct(request, pk):
     product = Product.objects.get(_id=pk)
     serializer = ProductSerializer(product, many=False)
     return Response(serializer.data)
+
+@api_view(['POST'])
+@permission_classes([IsAdminUser])
+def createProduct(request):
+    user = request.user
+    product = Product.objects.create(
+        user=user,
+        name='sample name',
+        price=0,
+        brand='sample brand',
+        countInStock=0,
+        category='Sample category',
+        description=''
+    )
+    serializer = ProductSerializer(product, many=False)
+    return Response(serializer.data)
+
+@api_view(['PUT'])
+@permission_classes([IsAdminUser])
+def updateProduct(request, pk):
+# date from the form 
+    data = request.data
+    product = Product.objects.get(_id=pk)
+
+    product.name = data['name']
+    product.price = data['price']
+    product.brand = data['brand']
+    product.countInStock = data['countInStock']
+    product.category = data['category']
+    product.description= data['description']
+
+    product.save()
+    
+    serializer = ProductSerializer(product, many=False)
+    return Response(serializer.data)
+
+@api_view(['DELETE'])
+@permission_classes([IsAdminUser])
+def deleteProduct(request, pk):
+    product = Product.objects.get(_id=pk)
+    product.delete()
+    return Response('Product wad deleted.')
