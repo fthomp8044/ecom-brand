50589b2ab1a35de457b147a88887d2074f7bcbb6
fthomp8044 <fthomp2430@gmail.com>
2021-06-18 21:08:35 -0400

    created the create review endpoint in backend

--- backend/base/views/product_views.py
+++ backend/base/views/product_views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
+from rest_framework import response
 
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
 from rest_framework.response import Response
 
-from base.models import Product
+from base.models import Product, Review
 
 from base.serializer import ProductSerializer
 
@@ -76,3 +77,57 @@ def deleteProduct(request, pk):
     product = Product.objects.get(_id=pk)
     product.delete()
     return Response('Product wad deleted.')
+
+
+@api_view(['POST'])
+def uploadImage(request):
+    data = request.data
+
+    product_id = data['product_id']
+    product = Product.objects.get(_id=product_id)
+    # take in the image from the file
+    product.image = request.FILES.get('image')
+    product.save()
+    return Response('Image was uploaded')
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def createProductReview(request, pk):
+    user = request.user
+    product = Product.objects.get(_id=pk)
+    data = request.data
+
+    # checkk if User already exists
+
+    already_exists = product.review_set.filter(user=user).exists()
+
+    if already_exists:
+        content = {'details': 'Review already exists'}
+        return Response(content, status=status.HTTP_400_BAD_REQUEST)
+
+    # if user leaves no rating
+    elif data['rating'] == 0:
+        content = {'details': 'Review has no rating'}
+        return Response(content, status=status.HTTP_400_BAD_REQUEST)
+        
+    # Create the review
+    else:
+        review = Review.objects.create(
+            user = user,
+            product=product,
+            name= user.fist_name,
+            rating=data['rating'],
+            comment=data['comment'],
+        )
+
+        # querryset to total up number of reviews
+        reviews = product.review_set.all()
+        product.numReviews = len(reviews)
+
+        total = 0
+        for i in reviews:
+            total += i.rating
+        product.rating = total / len(reviews)
+        product.save()
+
+        return Response({'Review added'})
