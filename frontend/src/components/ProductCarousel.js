e0f6bdd40e82ef2205f5811b3ff63347e89c5c62
fthomp8044 <fthomp2430@gmail.com>
2021-06-23 10:26:01 -0400

    finished completion of the carousel

--- frontend/src/components/ProductCarousel.js
+++ frontend/src/components/ProductCarousel.js
@@ -2,7 +2,7 @@ import React, {useEffect} from 'react'
 import { useDispatch, useSelector} from 'react-redux'
 import { Link } from 'react-router-dom'
 import { Carousel, Image} from 'react-bootstrap'
-import Loader from './loader'
+import Loader from './Loader'
 import Message from './Message'
 import { listTopProducts } from '../actions/productActions'
 
@@ -16,9 +16,22 @@ function ProductCarousel() {
         dispatch(listTopProducts())
     }, [dispatch])
     return (
-        <div>
-            
-        </div>
+        loading ? <Loader />
+        : error ? <Message variant='danger'>{error}</Message>
+        : (
+            <Carousel pause='hover' className='bg-dark'>
+                {products.map(product => (
+                    <Carousel.Item key={product._id}>
+                        <Link to={`/product/${product._id}`}>
+                            <Image src={product.image} alt={product.name} fluid/>
+                            <Carousel.Caption className='carousel.caption'>
+                                <h4>{product.name} ${product.price}</h4>
+                            </Carousel.Caption>
+                        </Link>
+                    </Carousel.Item>
+                ))}
+            </Carousel>
+        )
     )
 }
 
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -6,6 +6,7 @@ import Product from '../components/Product'
 import Loader from '../components/Loader'
 import Message from '../components/Message'
 import Paginate from '../components/Paginate'
+import ProductCarousel from '../components/ProductCarousel'
 
 // IMPORT ACTION TO SHOW THE LIST OF DATA
 import { listProducts } from '../actions/productActions'
@@ -25,6 +26,8 @@ function HomeScreen({history}) {
 
     return (
         <div>
+            {!keyword && <ProductCarousel />}
+            
             <h1>Latest Products</h1>
             {loading ? <Loader />
                 : error ? <Message variant='danger'>{error}</Message>
